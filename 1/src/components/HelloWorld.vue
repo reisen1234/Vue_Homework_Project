975f227eab0c992e85b7c461ddfef43abbb180a2
纳西妲单推人 <2843542905@qq.com>
2023-09-17 14:20:48 +0800

    Add files via upload

---
+++ 1/src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<!-- eslint-disable vue/no-parsing-error -->
+<template>
+  <div>
+    <p>
+    <h1>普通的button:</h1>
+    <button>button</button>
+    </p>
+    <p>
+    <h1>element-plus的button:</h1>
+    <el-button>Default</el-button>
+    </p>
+  </div>
+</template>
+
+<script>
+  import { ElButton } from 'element-plus'
+  export default {
+    components: { ElButton },
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
