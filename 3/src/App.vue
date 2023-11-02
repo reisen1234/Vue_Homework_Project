93b17bc7f5ba980b3de08057a31e04c1ea76fb64
纳西妲单推人 <2843542905@qq.com>
2023-11-02 10:15:34 +0800

    Add files via upload

---
+++ 3/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import myHome from "./views/Home/exerciseTwo.vue"
+</script>
+
+<template>
+  <myHome />
+</template>
+
+<style>
+#app {
+  width: 100%;
+  height: 100%;
+}
+</style>
