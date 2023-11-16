d8e27cce3a45f8f3d23990f408d8a4968a190f90
纳西妲单推人 <2843542905@qq.com>
2023-11-16 22:55:46 +0800

    Add files via upload

---
+++ 5/src/views/Home/homePage.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        This is home
+        <div>
+            <h1>用户信息</h1>
+            <p>用户ID: {{ userId }}</p>
+            <p>用户名: {{ username }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            userId: null,
+            username: null,
+        };
+    },
+    beforeRouteUpdate(to, from, next) {
+        console.log("beforeRouteUpdate triggered");
+        this.userId = to.params.id;
+        this.username = to.params.username;
+        next();
+    },
+    
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ 5/src/views/Home/myRouter.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        This is myRouter page
+        <button @click="switchs">切换路由</button>
+        <router-link :to="{ name: 'home', params: { id: 1, username: 'user1' } }">User 1</router-link>
+        <router-link :to="{ name: 'home', params: { id: 2, username: 'user2' } }">User 2</router-link>
+        <button @click="navigateToUser(3, 'user3')">User 3</button>
+        <p>
+            <router-link :to="{ name: 'register' }">register</router-link><br/>
+            <router-link :to="{ name: 'login' }">login</router-link>
+        </p>
+    </div>
+</template>
+  
+<script>
+export default {
+    methods: {
+        switchs() {
+            this.$router.push({ name: 'home', params: { id: 4, username: 'user4' } });
+        },
+        navigateToUser(id, username) {
+            console.log("User button clicked");
+            this.$router.push({ name: 'home', params: { id, username } });
+        },
+    },
+};
+</script>
+  
