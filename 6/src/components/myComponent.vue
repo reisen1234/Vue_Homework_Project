7087e018e05ab6adcad0d860894b7b9fd24f6cca
纳西妲单推人 <2843542905@qq.com>
2023-11-16 22:56:20 +0800

    Add files via upload

---
+++ 6/src/components/myComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <!-- <subComponent @custom-event="handleCustomEvent" />
+        <subComponent :numberProp="120"/> -->
+        <subComponent v-model="parentValue" @update:value="handleUpdateValue" />
+        {{ parentValue }}
+    </div>
+</template>
+
+<script >
+import subComponent from './subComponent.vue';
+export default {
+    data() {
+        return {
+            parentValue: 'Initial value from parent',
+        }
+    },
+    components: {
+        subComponent
+    },
+    methods: {
+        handleUpdateValue(newValue) {
+            // 处理子组件触发的事件
+            console.log('Received Update:', newValue);
+        },
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ 6/src/views/Home/myRouter.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        This is myRouter page
+        <button @click="switchs">切换路由</button>
+        <router-link :to="{ name: 'home', params: { id: 1, username: 'user1' } }">User 1</router-link>
+        <router-link :to="{ name: 'home', params: { id: 2, username: 'user2' } }">User 2</router-link>
+        <button @click="navigateToUser(3, 'user3')">User 3</button>
+        <p>
+            <router-link :to="{ name: 'register' }">register</router-link><br/>
+            <router-link :to="{ name: 'login' }">login</router-link>
+        </p>
+    </div>
+</template>
+  
+<script>
+export default {
+    methods: {
+        switchs() {
+            this.$router.push({ name: 'home', params: { id: 4, username: 'user4' } });
+        },
+        navigateToUser(id, username) {
+            console.log("User button clicked");
+            this.$router.push({ name: 'home', params: { id, username } });
+        },
+    },
+    /*     beforeRouteUpdate(to, from, next) {
+            console.log("beforeRouteUpdate triggered");
+            this.userId = to.params.id;
+            this.username = to.params.username;
+            next();
+        }, */
+    /* beforeRouteLeave(to, from, next) {
+        // 在用户离开当前路由时执行逻辑
+        // 可以进行一些清理操作或提醒用户
+        if (confirm("Are you sure you want to leave this page?")) {
+            next(); // 用户确认后继续导航
+        } else {
+            next(false); // 取消导航
+        }
+    } */
+};
+</script>
+  
